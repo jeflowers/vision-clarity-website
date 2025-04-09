7fdf319619cfdae323e9f95f1b1be105921d264c
John Flowers <jeflowers@gmail.com>
2025-04-08 20:10:42 -0500

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -54,7 +54,7 @@ class VisionClarityChatbot {
         chatbotButton.className = 'chatbot-button';
         chatbotButton.innerHTML = `
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2">
-                <path d="M21 12a9 9 0 0 1-9 9m9-9a9 9 0 0 0-9-9m9 9H3m9 9a9 9 0 0 1-9-9m9 9c1.66 0 3-4.03 3-9s-1.34-9-3-9m0 18c-1.66 0-3-4.03-3-9s1.34-9 3-9"/>
+                <path d="M21 12a9 9 0 0 1-9 9m9-9a9 9 0 0 0-9-9m9 9H3m9 9a9 9 0 0 1-9-9m9 9c1.66 0 3-4.03 3-9s-1.34-9-3-9m0 18c-1.66 0-3-4.03-3-9s1.34-9 3-9"></path>
             </svg>
         `;
         
