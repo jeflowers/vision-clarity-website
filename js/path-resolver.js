4b7f48892d1ef551c2222cdb91c220bf15ce98a8
John Flowers <jeflowers@gmail.com>
2025-04-15 13:59:11 -0500

    Update path-resolver.js

--- js/path-resolver.js
+++ js/path-resolver.js
@@ -3,11 +3,26 @@
  * Handles dynamic path resolution for assets across the site
  */
 const PathResolver = {
+  // Debug mode - set to false by default
+  debug: false,
+  
+  // Enable/disable debug logging
+  setDebug: function(enabled) {
+    this.debug = enabled;
+  },
+  
+  // Log function that only outputs when debug is enabled
+  log: function(message) {
+    if (this.debug) {
+      console.log(message);
+    }
+  },
+  
   // Get the root path based on current page location
   getRootPath: function() {
     // Get the current path
     const path = window.location.pathname;
-    console.log('Current pathname:', path);
+    this.log('Current pathname: ' + path);
     
     // Check if we're at the root or index.html
     if (path === '/' || path === '/index.html' || path.endsWith('/index.html')) {
@@ -21,30 +36,31 @@ const PathResolver = {
   // Get path to CSS files
   getCssPath: function(filename) {
     const cssPath = this.getRootPath() + 'css/' + filename;
-    console.log('CSS path resolved:', cssPath);
+    this.log('CSS path resolved: ' + cssPath);
     return cssPath;
   },
   
   // Get path to JS files
   getJsPath: function(filename) {
     const jsPath = this.getRootPath() + 'js/' + filename;
-    console.log('JS path resolved:', jsPath);
+    this.log('JS path resolved: ' + jsPath);
     return jsPath;
   },
   
   // Get path to components
   getComponentPath: function(filename) {
     const componentPath = this.getRootPath() + 'components/' + filename;
-    console.log('Component path resolved:', componentPath);
+    this.log('Component path resolved: ' + componentPath);
     return componentPath;
   },
   
   // Get path to assets
   getAssetPath: function(filename) {
     const assetPath = this.getRootPath() + 'assets/' + filename;
-    console.log('Asset path resolved:', assetPath);
+    this.log('Asset path resolved: ' + assetPath);
     return assetPath;
   }
 };
+
 // Make path resolver globally available
 window.PathResolver = PathResolver;
